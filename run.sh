e23606d418035425cfe23fa14f93ffdb1cfa1de8
Maurice Nonnekes <maurice@codeninja.nl>
2018-03-19 19:55:35 +0100

    Use wait-for to delay the pubsubc program

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 # Start the PubSub client in the background. It will poll for an open PubSub
 # emulator port and create its topics and subscriptions when it's up.
-env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug -wait="localhost:8681" &
+/usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug &
 
 # Start the PubSub emulator in the foreground.
 gcloud beta emulators pubsub start --host-port=0.0.0.0:8681
