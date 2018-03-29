b2a19c76427191a6c7d20d45d5b642d3ad0bc9b9
Maurice Nonnekes <maurice@codeninja.nl>
2018-03-29 09:19:46 +0200

    Open a port when the topics and subscriptions have been created

--- run.sh
+++ run.sh
@@ -2,7 +2,10 @@
 
 # Start the PubSub client in the background. It will poll for an open PubSub
 # emulator port and create its topics and subscriptions when it's up.
-/usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug &
+#
+# After it's done, port 8682 will be open to facilitate the wait-for and
+# wait-for-it scripts.
+(/usr/bin/wait-for localhost:8681 -- env PUBSUB_EMULATOR_HOST=localhost:8681 /usr/bin/pubsubc -debug; nc -lkp 8682 >/dev/null) &
 
 # Start the PubSub emulator in the foreground.
 gcloud beta emulators pubsub start --host-port=0.0.0.0:8681
